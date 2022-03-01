3992c00e9ccacb4ebce0b30ec051d4db9653df3e
asya-code <77514066+asya-code@users.noreply.github.com>
2022-02-28 19:19:06 -0800

    transformed melon_countring.py into melonCounting.js

---
+++ melonCounting.js
@@ -0,0 +1,24 @@
+'use strict';
+const melonsToAadd = ['Ogen', 'Horned Melon', 'Watermelon', 'Casaba',
+                 'Sharlyn', 'Xigua', 'Ogen', 'Christmas', 'Christmas',
+                 'Christmas', 'Christmas', 'Watermelon', 'Sharlyn', 'Xigua',
+                 'Cantaloupe', 'Christmas', 'Watermelon', 'Christmas',
+                 'Sharlyn', 'Christmas', 'Cantaloupe', 'Casaba', 'Cantaloupe',
+                 'Santa Claus', 'Horned Melon', 'Watermelon', 'Ogen',
+                 'Horned Melon', 'Cantaloupe', 'Xigua', 'Horned Melon', 'Sharlyn',
+                 'Horned Melon', 'Sharlyn', 'Cantaloupe', 'Christmas',
+                 'Horned Melon', 'Horned Melon', 'Horned Melon', 'Xigua', 'Xigua',
+                 'Watermelon', 'Cantaloupe', 'Casaba', 'Cantaloupe', 'Casaba',
+                 'Watermelon', 'Santa Claus', 'Casaba'];
+function countMelons(melonArray) {
+    const melonCounts = {};
+    for (const melon of melonArray) {
+        if (melonCounts[melon]) {
+            melonCounts[melon] += 1;
+        } else {
+            melonCounts[melon] = 1;
+            }
+        }
+    return melonCounts;
+    }
+console.log(countMelons(melonsToAadd));
